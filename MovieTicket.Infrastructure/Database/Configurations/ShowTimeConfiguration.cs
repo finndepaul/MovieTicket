c790c5342832ddaf2b53adbedbc82aa287c50e79
SuRinRin <dangnguyen300708@gmail.com>
2024-08-03 22:19:15 +0700

    sua db

--- MovieTicket.Infrastructure/Database/Configurations/ShowTimeConfiguration.cs
+++ MovieTicket.Infrastructure/Database/Configurations/ShowTimeConfiguration.cs
@@ -14,12 +14,12 @@ namespace MovieTicket.Infrastructure.Database.Configurations
         public void Configure(EntityTypeBuilder<ShowTime> builder)
         {
             builder.HasKey(x => x.Id);
-            builder.HasOne(x => x.ScreenType).WithOne(x => x.ShowTime).HasForeignKey<ShowTime>(x => x.ScreenTypeId);
+            builder.HasOne(x => x.ScreenType).WithMany(x => x.ShowTime).HasForeignKey(x => x.ScreenTypeId);
             builder.HasOne(x=>x.Cinema).WithMany(x=>x.ShowTimes).HasForeignKey(x=>x.CinemaId);
             builder.HasOne(x=>x.CinemaCenter).WithMany(x=>x.ShowTimes).HasForeignKey(x=>x.CinemaCenterId);
             builder.HasOne(x=>x.Film).WithMany(x=>x.ShowTimes).HasForeignKey(x=>x.FilmId);
             builder.HasOne(x=>x.Schedule).WithMany(x=>x.ShowTimes).HasForeignKey(x=>x.ScheduleId);
-            builder.HasOne(x=>x.TranslationType).WithOne(x=>x.ShowTime).HasForeignKey<ShowTime>(x=>x.TranslationTypeId);
+            builder.HasOne(x=>x.TranslationType).WithMany(x=>x.ShowTime).HasForeignKey(x=>x.TranslationTypeId);
             
         }
     }
