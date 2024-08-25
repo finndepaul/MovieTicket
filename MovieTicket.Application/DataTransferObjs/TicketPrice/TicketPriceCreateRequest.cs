5b56102f974af3d2e5905d3e123d5a3bf9f8ecb1
SuRinRin <dangnguyen300708@gmail.com>
2024-08-25 22:34:37 +0700

    done ticket price

--- MovieTicket.Application/DataTransferObjs/TicketPrice/TicketPriceCreateRequest.cs
+++ MovieTicket.Application/DataTransferObjs/TicketPrice/TicketPriceCreateRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MovieTicket.Application.ValueObjs.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,17 +8,42 @@ using System.Threading.Tasks;
 
 namespace MovieTicket.Application.DataTransferObjs.TicketPrice
 {
-    public class TicketPriceCreateRequest
-    {
-        public Guid? SeatTypeId { get; set; }
+	public class TicketPriceCreateRequest
+	{
+		public Guid SeatTypeId { get; set; }
+		public Guid ScreenTypeId { get; set; }
+		public Guid ScreeningDayId { get; set; }
+		public Guid CinemaTypeId { get; set; }
+		public decimal Price { get; set; }
 
-        public Guid? ScreenTypeId { get; set; }
-
-        public Guid? ScreeningDayId { get; set; }
-
-        public Guid? CinemaTypeId { get; set; }
-
-        [Range(0,int.MaxValue,ErrorMessage = "Must be positive")]
-        public decimal? Price { get; set; }
-    }
+		public string Validate()
+		{
+			string message = "";
+			if (String.IsNullOrEmpty(SeatTypeId.ToString()))
+			{
+				message += "SeatTypeId is required. ";
+			}
+			if (String.IsNullOrEmpty(ScreenTypeId.ToString()))
+			{
+				message += "ScreenTypeId is required. ";
+			}
+			if (String.IsNullOrEmpty(ScreeningDayId.ToString()))
+			{
+				message += "ScreeningDayId is required. ";
+			}
+			if (String.IsNullOrEmpty(CinemaTypeId.ToString()))
+			{
+				message += "CinemaTypeId is required. ";
+			}
+			if (String.IsNullOrEmpty(Price.ToString()))
+			{
+				message += "Price is required. ";
+			}
+			if (Price <= 20000)
+			{
+				message += "Price must be more than 20000. ";
+			}
+			return message;
+		}
+	}
 }
