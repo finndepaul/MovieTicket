968436636f08946df7116873cce638d85d8e9e91
dong bui <dongbui.work@gmail.com>
2024-10-09 23:12:46 +0700

    chua chay 1

--- MovieTicket.Domain/Entities/Schedule.cs
+++ MovieTicket.Domain/Entities/Schedule.cs
@@ -6,19 +6,19 @@ namespace MovieTicket.Domain.Entities;
 
 public  class Schedule
 {
-    public Guid Id { get; set; }
+    public Guid? Id { get; set; }
 
-    public Guid FilmId { get; set; }
+    public Guid? FilmId { get; set; }
 
-    public DateTime StartDate { get; set; }
+    public DateTime? StartDate { get; set; }
 
-    public DateTime EndDate { get; set; }
+    public DateTime? EndDate { get; set; }
 
-    public ScheduleType Type { get; set; }
+    public ScheduleType? Type { get; set; }
 
-    public ScheduleStatus Status { get; set; }
+    public ScheduleStatus? Status { get; set; }
     //public bool? ActivationStatus { get; set; }
 
-    public virtual Film Film { get; set; }
-    public virtual ICollection<ShowTime> ShowTimes { get; set; }
+    public virtual Film? Film { get; set; }
+    public virtual ICollection<ShowTime>? ShowTimes { get; set; }
 }
