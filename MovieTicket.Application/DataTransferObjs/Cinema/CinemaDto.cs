ab49d98583aee354ffc06d547bd20c04a99be2d3
isora666 <121970732+isora666@users.noreply.github.com>
2024-10-13 21:41:15 +0700

    FE Cinema Vippro

--- MovieTicket.Application/DataTransferObjs/Cinema/CinemaDto.cs
+++ MovieTicket.Application/DataTransferObjs/Cinema/CinemaDto.cs
@@ -18,5 +18,6 @@ namespace MovieTicket.Application.DataTransferObjs.Cinema
         public int Row { get; set; }
         public string Description { get; set; }
         public DateTime CreateTime { get; set; }
+        public DateTime? UpdateTime { get; set; }
     }
 }
---
+++ MovieTicket.BlazorServer/Services/Interfaces/ICinemaService.cs
@@ -0,0 +1,12 @@
+﻿using MovieTicket.Application.DataTransferObjs.Cinema;
+using MovieTicket.Application.ValueObjs.ViewModels;
+
+namespace MovieTicket.BlazorServer.Services.Interfaces
+{
+    public interface ICinemaService
+    {
+        Task<List<CinemaDto>> GetCinemasAsync(string? name);
+
+        Task<ResponseObject<CinemaDto>> GetCinemaById(Guid id);
+    }
+}
