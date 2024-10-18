e7afed6be29d6d14e37552840e37f037b33d715f
Dong-Toan2004 <149890821+Dong-Toan2004@users.noreply.github.com>
2024-10-18 19:56:02 +0700

    Done show time

--- MovieTicket.Infrastructure/Implements/Repositories/ReadOnly/FilmReadOnlyRepostitory.cs
+++ MovieTicket.Infrastructure/Implements/Repositories/ReadOnly/FilmReadOnlyRepostitory.cs
@@ -5,64 +5,92 @@ using MovieTicket.Infrastructure.Database.AppDbContexts;
 
 namespace MovieTicket.Infrastructure.Implements.Repositories.ReadOnly
 {
-    public class FilmReadOnlyRepostitory : IFilmReadOnlyRepository
-    {
-        private readonly MovieTicketReadOnlyDbContext _context;
+	public class FilmReadOnlyRepostitory : IFilmReadOnlyRepository
+	{
+		private readonly MovieTicketReadOnlyDbContext _context;
 
-        public FilmReadOnlyRepostitory(MovieTicketReadOnlyDbContext movieTicketReadOnlyDbContext)
-        {
-            _context = movieTicketReadOnlyDbContext;
-        }
+		public FilmReadOnlyRepostitory(MovieTicketReadOnlyDbContext movieTicketReadOnlyDbContext)
+		{
+			_context = movieTicketReadOnlyDbContext;
+		}
 
-        public async Task<IQueryable<FilmDto>> GetAllFilm()
-        {
-            // getall film
-            var films = _context.Films.Select(film => new FilmDto
-            {
-                Id = film.Id,
-                Name = film.Name,
-                EnglishName = film.EnglishName,
-                Trailer = film.Trailer,
-                Description = film.Description,
-                Gerne = film.Gerne,
-                Director = film.Director,
-                Cast = film.Cast,
-                Rating = film.Rating,
-                StartDate = film.StartDate,
-                ReleaseYear = film.ReleaseYear,
-                RunningTime = film.RunningTime,
-                Status = film.Status,
-                Nation = film.Nation,
-                Poster = film.Poster,
-                Language = film.Language,
-                CreatDate = film.CreatDate
-            });
-            return films;
-        }
+		public async Task<IQueryable<FilmDto>> GetAllFilm()
+		{
+			//         // getall film
+			//         var films = _context.Films.Select(film => new FilmDto
+			//         {
+			//             Id = film.Id,
+			//             Name = film.Name,
+			//             EnglishName = film.EnglishName,
+			//             Trailer = film.Trailer,
+			//             Description = film.Description,
+			//             Gerne = film.Gerne,
+			//             Director = film.Director,
+			//             Cast = film.Cast,
+			//             Rating = film.Rating,
+			//             StartDate = film.StartDate,
+			//             ReleaseYear = film.ReleaseYear,
+			//             RunningTime = film.RunningTime,
+			//             Status = film.Status,
+			//             Nation = film.Nation,
+			//             Poster = film.Poster,
+			//             Language = film.Language,
+			//             CreatDate = film.CreatDate,
+			//             ScreenTypeIds = new List<Guid>(),
+			//});
+			var query = _context.Films
+				  .Join(_context.FilmScreenTypes, f => f.Id, fst => fst.FilmId, (f, fst) => new { f, fst })
+				  .Join(_context.FilmTranslationTypes, x => x.f.Id, ftt => ftt.FilmId, (x, ftt) => new { x.fst, x.f, ftt })
+				  .GroupBy(x => new { x.f.Id, x.f.Name, x.f.EnglishName, x.f.Trailer, x.f.Description, x.f.Gerne, x.f.Director, x.f.Cast, x.f.Rating, x.f.StartDate, x.f.ReleaseYear, x.f.RunningTime, x.f.Status, x.f.Nation, x.f.Poster, x.f.Language, x.f.CreatDate })
+				  .Select(group => new FilmDto()
+				  {
+					  Id = group.Key.Id,
+					  Name = group.Key.Name,
+					  EnglishName = group.Key.EnglishName,
+					  Trailer = group.Key.Trailer,
+					  Description = group.Key.Description,
+					  Gerne = group.Key.Gerne,
+					  Director = group.Key.Director,
+					  Cast = group.Key.Cast,
+					  Rating = group.Key.Rating,
+					  StartDate = group.Key.StartDate,
+					  ReleaseYear = group.Key.ReleaseYear,
+					  RunningTime = group.Key.RunningTime,
+					  Status = group.Key.Status,
+					  Nation = group.Key.Nation,
+					  Poster = group.Key.Poster,
+					  Language = group.Key.Language,
+					  CreatDate = group.Key.CreatDate,
+					  ScreenTypeIds = group.Select(g => g.fst.ScreenTypeId).Distinct().ToList(), // Lấy danh sách ScreenTypeIds
+					  TranslationTypeIds = group.Select(g => g.ftt.TranslationTypeId).Distinct().ToList() // Lấy danh sách TranslationTypeIds
+				  }).AsQueryable();
 
-        public async Task<FilmDto> GetFilmById(Guid id)
-        {
-            var film = _context.Films.Where(film => film.Id == id).Select(film => new FilmDto
-            {
-                Id = film.Id,
-                Name = film.Name,
-                EnglishName = film.EnglishName,
-                Trailer = film.Trailer,
-                Description = film.Description,
-                Gerne = film.Gerne,
-                Director = film.Director,
-                Cast = film.Cast,
-                Rating = film.Rating,
-                StartDate = film.StartDate,
-                ReleaseYear = film.ReleaseYear,
-                RunningTime = film.RunningTime,
-                Status = film.Status,
-                Nation = film.Nation,
-                Poster = film.Poster,
-                Language = film.Language,
-                CreatDate = film.CreatDate
-            }).FirstOrDefault();
-            return film;
-        }
-    }
+			return query;
+		}
+
+		public async Task<FilmDto> GetFilmById(Guid id)
+		{
+			var film = _context.Films.Where(film => film.Id == id).Select(film => new FilmDto
+			{
+				Id = film.Id,
+				Name = film.Name,
+				EnglishName = film.EnglishName,
+				Trailer = film.Trailer,
+				Description = film.Description,
+				Gerne = film.Gerne,
+				Director = film.Director,
+				Cast = film.Cast,
+				Rating = film.Rating,
+				StartDate = film.StartDate,
+				ReleaseYear = film.ReleaseYear,
+				RunningTime = film.RunningTime,
+				Status = film.Status,
+				Nation = film.Nation,
+				Poster = film.Poster,
+				Language = film.Language,
+				CreatDate = film.CreatDate
+			}).FirstOrDefault();
+			return film;
+		}
+	}
 }
--- MovieTicket.Infrastructure/Implements/Repositories/ReadWrite/ShowTimeReadWriteRepository.cs
+++ MovieTicket.Infrastructure/Implements/Repositories/ReadWrite/ShowTimeReadWriteRepository.cs
@@ -10,22 +10,22 @@ using MovieTicket.Infrastructure.Database.AppDbContexts;
 
 namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 {
-    public class ShowTimeReadWriteRepository : IShowTimeReadWriteRepository
-    {
-        private readonly MovieTicketReadWriteDbContext _db;
-        private readonly IMapper _mapper;
+	public class ShowTimeReadWriteRepository : IShowTimeReadWriteRepository
+	{
+		private readonly MovieTicketReadWriteDbContext _db;
+		private readonly IMapper _mapper;
 
-        public ShowTimeReadWriteRepository(MovieTicketReadWriteDbContext db, IMapper mapper)
-        {
-            _db = db;
-            _mapper = mapper;
-        }
+		public ShowTimeReadWriteRepository(MovieTicketReadWriteDbContext db, IMapper mapper)
+		{
+			_db = db;
+			_mapper = mapper;
+		}
 
 		public async Task<ResponseObject<ShowTime>> Create(ShowTimeCreateRequest showTime)
 		{
 			// Tìm thời gian kết thúc của suất chiếu cuối cùng trong ngày tại cùng rạp và loại màn hình
 			var lastShowTime = await _db.ShowTimes
-				.Where(st => st.CinemaId == showTime.CinemaId && st.ScreenTypeId == showTime.ScreenTypeId && st.ShowtimeDate.Value == showTime.ShowtimeDate.Value)
+				.Where(st => st.CinemaId == showTime.CinemaId && st.ShowtimeDate.Value == showTime.ShowtimeDate.Value)
 				.OrderByDescending(st => st.EndTime)
 				.FirstOrDefaultAsync();
 
@@ -34,19 +34,12 @@ namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 				.Where(x => x.FilmId == showTime.FilmId && (x.Status == ScheduleStatus.ComingSoon || x.Status == ScheduleStatus.Showing))
 				.FirstOrDefaultAsync();
 
-			// Tìm suất chiếu có thời gian trùng nhau trong cùng một ngày
-			var overlappingShowTime = await _db.ShowTimes
-				.Where(st => st.CinemaId == showTime.CinemaId && st.ScreenTypeId == showTime.ScreenTypeId && st.ShowtimeDate.Value == showTime.ShowtimeDate.Value
-							 && ((showTime.StartTime >= st.StartTime && showTime.StartTime <= st.EndTime)
-								  || (showTime.EndTime >= st.StartTime && showTime.EndTime <= st.EndTime)))
-				.FirstOrDefaultAsync();
-
 			// Check null các trường bắt buộc
 			if (!showTime.FilmId.HasValue || !showTime.ScheduleId.HasValue ||
 				!showTime.CinemaCenterId.HasValue || !showTime.CinemaId.HasValue ||
 				!showTime.ScreenTypeId.HasValue || !showTime.TranslationTypeId.HasValue ||
 				!showTime.StartTime.HasValue || !showTime.EndTime.HasValue ||
-				string.IsNullOrEmpty(showTime.Desciption) || !showTime.Status.HasValue)
+				string.IsNullOrEmpty(showTime.Desciption))
 			{
 				return new ResponseObject<ShowTime>
 				{
@@ -56,16 +49,16 @@ namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 				};
 			}
 
-			// Check thời gian bắt đầu và kết thúc của suất chiếu
-			if (showTime.EndTime <= showTime.StartTime)
-			{
-				return new ResponseObject<ShowTime>
-				{
-					Data = null,
-					Message = "Lỗi thời gian bắt đầu và kết thúc",
-					Status = StatusCodes.Status400BadRequest
-				};
-			}
+			// Tìm suất chiếu có thời gian trùng nhau trong cùng một ngày
+			var overlappingShowTime = await _db.ShowTimes
+				.Where(st => st.CinemaId == showTime.CinemaId
+							 && st.ShowtimeDate.Value == showTime.ShowtimeDate.Value
+							 && ((st.ScreenTypeId == showTime.ScreenTypeId && st.TranslationTypeId == showTime.TranslationTypeId)
+								 || (st.ScreenTypeId != showTime.ScreenTypeId && st.TranslationTypeId == showTime.TranslationTypeId)
+								 || (st.ScreenTypeId == showTime.ScreenTypeId && st.TranslationTypeId != showTime.TranslationTypeId))
+							 && ((showTime.StartTime >= st.StartTime && showTime.StartTime <= st.EndTime)
+								 || (showTime.EndTime >= st.StartTime && showTime.EndTime <= st.EndTime)))
+				.FirstOrDefaultAsync();
 
 			// Check thời gian suất chiếu không chồng chéo với suất chiếu khác trong cùng ngày
 			if (overlappingShowTime != null)
@@ -93,17 +86,6 @@ namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 				}
 			}
 
-			// Check thời gian của suất chiếu phải nằm trong khoảng thời gian của lịch chiếu
-			if (schedule != null && (showTime.StartTime.Value < schedule.StartDate || showTime.EndTime.Value > schedule.EndDate))
-			{
-				return new ResponseObject<ShowTime>
-				{
-					Data = null,
-					Message = "Giờ chiếu phải nằm trong khung thời gian đã định",
-					Status = StatusCodes.Status400BadRequest
-				};
-			}
-
 			// Tạo suất chiếu mới
 			var newShowTime = new ShowTime
 			{
@@ -118,7 +100,7 @@ namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 				EndTime = showTime.EndTime.Value,
 				ShowtimeDate = showTime.ShowtimeDate ?? DateTime.Today,
 				Desciption = showTime.Desciption,
-				Status = showTime.Status.Value
+				Status = showTime.Status
 			};
 
 			_db.ShowTimes.Add(newShowTime);
@@ -133,26 +115,26 @@ namespace MovieTicket.Infrastructure.Implements.Repositories.ReadWrite
 		}
 
 		public async Task<ResponseObject<ShowTime>> Delete(Guid id)
-        {
-            var showTime = await _db.ShowTimes.FindAsync(id);
-            if (showTime == null)
-            {
-                return new ResponseObject<ShowTime>
-                {
-                    Data = null,
-                    Message = "ShowTime not found",
-                    Status = StatusCodes.Status404NotFound
-                };
-            }
-            _db.ShowTimes.Remove(showTime);
-            await _db.SaveChangesAsync();
-            return new ResponseObject<ShowTime>
-            {
-                Data = showTime,
-                Message = "Delete success",
-                Status = StatusCodes.Status200OK
-            };
-        }
+		{
+			var showTime = await _db.ShowTimes.FindAsync(id);
+			if (showTime == null)
+			{
+				return new ResponseObject<ShowTime>
+				{
+					Data = null,
+					Message = "ShowTime not found",
+					Status = StatusCodes.Status404NotFound
+				};
+			}
+			_db.ShowTimes.Remove(showTime);
+			await _db.SaveChangesAsync();
+			return new ResponseObject<ShowTime>
+			{
+				Data = showTime,
+				Message = "Delete success",
+				Status = StatusCodes.Status200OK
+			};
+		}
 
-    }
+	}
 }
