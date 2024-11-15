5ed2cf80803f3b1bd2fce1512743daa4ecb510bb
Finn de paul <dongbui.work@gmail.com>
2024-11-15 08:41:11 +0700

    luong thanh toan thanh cong (chua chon combo va giam gia)

--- MovieTicket.Application/DataTransferObjs/Bill/BillDetailDto.cs
+++ MovieTicket.Application/DataTransferObjs/Bill/BillDetailDto.cs
@@ -1,22 +1,25 @@
-﻿using MovieTicket.Domain.Enums;
+﻿using MovieTicket.Application.DataTransferObjs.Combo;
+using MovieTicket.Domain.Enums;
 
 namespace MovieTicket.Application.DataTransferObjs.Bill
 {
-	public class BillDto
+	public class BillDetailDto
 	{
 		public Guid Id { get; set; }
 		//public Guid? MembershipId { get; set; }
 
 		//public Guid? VoucherId { get; set; }
-
-		public string FilmName { get; set; }
+		public int BillCode { get; set; }
 
 		public decimal? TotalMoney { get; set; }
+
+		public string FilmName { get; set; }
 
 		public DateTime? CreateTime { get; set; }
 
 		public string? BarCode { get; set; }
 
 		public BillStatus? Status { get; set; }
+		public List<ComboDto>? Combos { get; set; } = new List<ComboDto>();
 	}
 }
