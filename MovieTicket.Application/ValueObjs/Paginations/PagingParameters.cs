87365061fc4963e704486809cdbf71d963441b62
Finn de paul <dongbui.work@gmail.com>
2024-11-30 10:18:16 +0700

    fix coupon

--- MovieTicket.Application/ValueObjs/Paginations/PagingParameters.cs
+++ MovieTicket.Application/ValueObjs/Paginations/PagingParameters.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace MovieTicket.Application.ValueObjs.Paginations
 {
-	public class PagingParameters
-	{
-		public int PageNumber { get; set; } = 1; //Trang hiện tại
-		public int PageSize { get; set; } = 5; //Số lượng phần tử trên mỗi trang
-	}
+    public class PagingParameters
+    {
+        public int PageNumber { get; set; } = 1; //Trang hiện tại
+        public int PageSize { get; set; } = 10; //Số lượng phần tử trên mỗi trang
+    }
 }
