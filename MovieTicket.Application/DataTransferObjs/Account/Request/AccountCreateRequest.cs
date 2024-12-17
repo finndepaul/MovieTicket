c736ab39795ad377f8ed49956a8b21c0429b8368
VuHope <lienminhhuyenthoai1379@gmail.com>
2024-12-17 16:20:01 +0700

    Done user history

--- MovieTicket.Application/DataTransferObjs/Account/Request/AccountCreateRequest.cs
+++ MovieTicket.Application/DataTransferObjs/Account/Request/AccountCreateRequest.cs
@@ -5,22 +5,22 @@ namespace MovieTicket.Application.DataTransferObjs.Account.Request
 {
     public class AccountCreateRequest
     {
-        [Required]
+        [Required(ErrorMessage = "Tên tài khoản là bắt buộc.")]
         [RegularExpression(@"^[a-zA-Z0-9_-]+$", ErrorMessage = "Tên người dùng không được chứa khoảng trắng hoặc ký tự đặc biệt và không được có dấu phụ.")]
         public string? Username { get; set; }
 
-        [Required]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số.")]
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc.")]
+        [RegularExpression(@"^.{8,32}$", ErrorMessage = "Mật khẩu phải từ 8 đến 32 ký tự.")]
         public string? Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Họ và tên là bắt buộc.")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Số điện thoại là bắt buộc.")]
         [RegularExpression(@"^0\d{9}$", ErrorMessage = "Địa chỉ phone sai định dạng !!!")]
         public string? Phone { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Địa chỉ Email là bắt buộc.")]
         [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Địa chỉ email sai định dạng !!!")]
         public string? Email { get; set; }
     }
